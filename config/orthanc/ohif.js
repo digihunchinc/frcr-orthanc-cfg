e0e4d5a87cc46f0084c275bd7a1dd9601a4a2df1
Yi Lu <ylu@digihunch.com>
2025-02-23 12:43:23 -0500

    update ohif viewer config

--- config/orthanc/ohif.js
+++ config/orthanc/ohif.js
@@ -1,8 +1,29 @@
+// https://orthanc.uclouvain.be/book/plugins/ohif.html
+
+
+
 window.config = {
+  // the following configuration options are overriden when using OHIF plugin for Orthanc
+  // routerBasename: '/', // window.config.dataSources
+  // dataSources: [], // window.config.dataSources
+  // defaultDataSourceName: 'dicomweb', // window.config.defaultDataSourceName
+  // showStudyList: false, //set to false if using the DICOM JSON data source
+  // The values are from Orthanc configuration for OHIF plugin.
+
   extensions: [],
   modes: [],
-  showStudyList: false,
   disableEditing: true,
-  investigationalUseDialog: 'never',
+  investigationalUseDialog: {
+    option: 'never',
+  },
   studyListFunctionsEnabled: false,
-}
+
+
+  httpErrorHandler: error => {
+    // This is 429 when rejected from the public idc sandbox too often.
+    console.warn(error.status);
+
+    // Could use services manager here to bring up a dialog/modal if needed.
+    console.warn('test, navigate to https://ohif.org/');
+  },
+};
